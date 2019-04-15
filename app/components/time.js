b6c61733869f3a40721d55dfb34a4fc428dd3059
Kiko Estrada <uldarico@outlook.com>
2019-04-14 23:42:03 -0700

    ✳️ Added timeline component

---
+++ app/components/time.js
@@ -0,0 +1,61 @@
+import moment from 'moment'
+import React from 'react'
+
+moment.locale(
+  'en-short',
+  {
+    relativeTime: {
+      d: "1d",
+      dd: "%dd",
+      future: "%s",
+      h: "1h",
+      hh: "%dh",
+      m: "1m",
+      M: "1M",
+      mm: "%dm",
+      MM: "%dM",
+      past: "%s",
+      s: "now",
+      y: "1y",
+      yy: "%dy",
+    },
+  },
+);
+
+export default class Time extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.now != nextProps.now;
+  }
+
+  getRelativeTime() {
+    return this.getMomentTime().locale('en-short').fromNow();
+  }
+
+  getAbsoluteTime() {
+    return this.getMomentTime().format('YYYY/MM/DD');
+  }
+
+  getDisplayedTime() {
+    if (this.getMomentTime().isBefore(moment().subtract(1, 'day'))) {
+      return this.getAbsoluteTime();
+    } else {
+      return this.getRelativeTime();
+    }
+  }
+
+  getDateTime() {
+    return this.getMomentTime().format('YYYY-MM-DDTHH:mm:ssZ');
+  }
+
+  getMomentTime() {
+    return moment(new Date(this.props.time));
+  }
+
+  render() {
+    return(
+      <time {...this.props} dateTime={this.getDateTime()} title={this.getAbsoluteTime()}>
+        {this.getDisplayedTime()}
+      </time>
+    );
+  }
+}
---
+++ app/components/timeline.js
@@ -0,0 +1,49 @@
+import React, { PropTypes } from 'react';
+import ListContainer        from '../containers/list-container';
+import MentionTabContainer  from '../containers/mention-tab-container';
+import FavoritesTabContainer from '../containers/favorites-tab-container';
+import SearchContainer      from '../containers/search-container';
+import TweetListContainer   from '../containers/tweet-list-container';
+import TweetTabContainer    from '../containers/tweet-tab-container';
+
+export default class Timeline extends React.Component {
+  static propTypes = {
+    account:        PropTypes.object.isRequired,
+    active:         PropTypes.bool.isRequired,
+    loadHome:       PropTypes.func.isRequired,
+    loadLists:      PropTypes.func.isRequired,
+    loadMentions:   PropTypes.func.isRequired,
+    pollHome:       PropTypes.func.isRequired,
+  }
+
+  componentDidMount() {
+    this.props.loadHome();
+    this.props.loadMentions();
+    this.props.loadFavorites();
+    this.props.loadLists();
+    this.props.pollHome();
+  }
+
+  render() {
+    return(
+      <div className={`timeline ${this.props.active ? 'active' : ''}`}>
+        <ul className='tabs clearfix'>
+          <TweetTabContainer account={this.props.account} tab='home'>Timeline</TweetTabContainer>
+          <MentionTabContainer account={this.props.account} tab='mentions'>Mentions</MentionTabContainer>
+          <TweetTabContainer account={this.props.account} tab='search'>Search</TweetTabContainer>
+          <TweetTabContainer account={this.props.account} tab='lists'>Lists</TweetTabContainer>
+          <FavoritesTabContainer account={this.props.account} tab='favorites'>Favorites</FavoritesTabContainer>
+        </ul>
+
+        <TweetListContainer account={this.props.account} tab='home' />
+        <TweetListContainer account={this.props.account} tab='mentions' />
+        <TweetListContainer account={this.props.account} tab='search' withHeader={true} />
+        <TweetListContainer account={this.props.account} tab='lists'  withHeader={true} />
+        <TweetListContainer account={this.props.account} tab='favorites' />
+
+        <ListContainer account={this.props.account} />
+        <SearchContainer account={this.props.account} />
+      </div>
+    );
+  }
+}
